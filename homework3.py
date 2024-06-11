2080953197fa2218a7c36639db8f4ba72e32bdd3
MaximLuchkin <porosh@bk.ru>
2024-06-11 23:51:56 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Maxim'
+print('Name' + ': ' + (name))
+age = 29
+print('Age' + ': ' + (str(age)))
+age = (age+2)
+print('New Age' + ': ' + (str(age)))
+is_student = True
+print('Is Student' + ': ' + str(is_student))
