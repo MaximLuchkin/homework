fabb0f73ce7ce09c0a420522e7d260d18ba6cf80
MaximLuchkin <porosh@bk.ru>
2024-06-17 08:32:00 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,9 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 for i, number in enumerate(my_list):
-    my_list_int = my_list[i]
-    if my_list_int < 0:
+    if my_list[i] < 0:
         break
-    if my_list_int == int(my_list[-1]):
-        print(my_list_int)
+    if my_list[i] == int(my_list[-1]):
+        print(my_list[i])
         break
-    print(my_list_int)
+    print(my_list[i])
 
